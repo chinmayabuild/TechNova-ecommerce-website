d13e6cdfd26a3de9e96720c73fb64c7266cf56c6
chinuuuuuu <chinmaydhal6370@gmail.com>
2025-03-21 22:04:57 +0530

    new update

--- client/src/component/home/Category.jsx
+++ client/src/component/home/Category.jsx
@@ -2,18 +2,18 @@ import { Button } from "../ui/button";
 
 const Category = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 w-[93vw]">
+    <div className=" flex justify-center items-center gap-6 p-4 w-[95vw] ">
 
       {/* Small Cards */}
-      <div className="md:col-span-1 flex flex-col md:flex-row gap-4">
+      <div className="md:col-span-1 flex flex-col md:flex-row gap-6  ">
         <div className="bg-black text-white p-6 rounded-2xl flex-1 relative">
-          <img src="https://via.placeholder.com/150" alt="Earphone" className="absolute right-2 top-2 w-24 h-24 object-cover" />
+          <img src="https://via.placeholder.com/150" alt="Earphone" className="absolute right-2 top-2 w-24 h-40 object-cover" />
           <p className="text-sm">Enjoy</p>
           <h2 className="text-xl font-bold">With Earphone</h2>
           <Button className="mt-4">Browse</Button>
         </div>
-        <div className="bg-yellow-400 text-black p-6 rounded-2xl flex-1 relative">
-          <img src="https://via.placeholder.com/150" alt="Gadget" className="absolute right-2 top-2 w-24 h-24 object-cover" />
+        <div className="bg-yellow-400 text-black p-6 rounded-2xl flex-1 relative h-[45vh]">
+          <img src="https://via.placeholder.com/150" alt="Gadget" className="absolute right-2 top-2 w-24 h-40 object-cover" />
           <p className="text-sm">New</p>
           <h2 className="text-xl font-bold">Wearable Gadget</h2>
           <Button className="mt-4">Browse</Button>
@@ -22,7 +22,7 @@ const Category = () => {
 
       {/* Large Card */}
       <div className="bg-[#D9190E] text-white p-6 rounded-2xl md:col-span-2 relative">
-        <img src="https://via.placeholder.com/250" alt="Laptop" className="absolute right-2 top-2 w-40 h-40 object-cover" />
+        <img src="https://via.placeholder.com/250" alt="Laptop" className="absolute right-2 top-2 w-[30vw] h-40 object-cover" />
         <p className="text-sm">Trend</p>
         <h2 className="text-2xl font-bold">Devices & Laptop</h2>
         <Button className="mt-4">Browse</Button>
--- client/src/component/productPage/ProductCard.jsx
+++ client/src/component/productPage/ProductCard.jsx
@@ -19,9 +19,8 @@ const ProductCard = ({
       <img
         src={image.url}
         alt={name}
-        className="object-cover w-[28rem] h-[30rem]  "
+        className="object-cover w-[28rem] h-[30rem]"
       />
-
       <div className="px-5 grid gap-2 py-2 absolute bg-white dark:bg-zinc-900 w-full bottom-0 translate-y-[3rem] hover:translate-y-0 transform transition-all ease-in-out rounded-xl duration-300">
         <h2>{name}</h2>
         <div className="flex justify-between">
